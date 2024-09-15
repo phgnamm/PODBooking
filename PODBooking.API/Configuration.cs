fad9a263c873ad9f351b8222d08861de1805565b
phgnam <kiss01646803595@gmail.com>
2024-09-15 10:19:05 +0700

    feat : Implement AppDBContext and appsetting

---
+++ PODBooking.API/Configuration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PODBooking.API.Middlewares;
+using PODBooking.API.Utils;
+using Repositories.Entities;
+using Repositories;
+using System.Diagnostics;
+
+namespace PODBooking.API
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddAPIConfiguration(this IServiceCollection services)
+        {
+            // Identity
+            services
+                .AddIdentity<Account, Role>(options =>
+                {
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequiredLength = 8;
+                })
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+            {
+                options.TokenLifespan = TimeSpan.FromMinutes(15);
+            });
+
+            // Middlewares
+            services.AddSingleton<GlobalExceptionMiddleware>();
+            services.AddSingleton<PerformanceMiddleware>();
+            services.AddScoped<AccountStatusMiddleware>();
+            services.AddSingleton<Stopwatch>();
+
+           /* // Common
+            services.AddHttpContextAccessor();
+            services.AddAutoMapper(typeof(MapperProfile).Assembly);
+            services.AddScoped<IClaimsService, ClaimsService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<IEmailService, EmailService>();
+
+            // Dependency Injection
+            // Account
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<IAccountRepository, AccountRepository>();*/
+
+            return services;
+        }
+    }
+}
--- PODBooking.API/Program.cs
+++ PODBooking.API/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Repositories;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,9 +10,44 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "PODBooking", Version = "v1" });
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+}
+);
 
+// Local Database
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDB"));
+});
 var app = builder.Build();
+
+// Add API Configuration
+builder.Services.AddAPIConfiguration();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
