28921fef848e084bb0acf1c5c4b6c86b231a116f
Thanh Dung <nguyenthanhdung2308@gmail.com>
2024-10-16 02:29:20 +0700

    update code

---
+++ PODBooking.API/Controllers/PaymentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using Services.Models.PaymentModels;
+using Services.Services;
+
+namespace PODBooking.API.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentGatewayService _paymentGatewayService;
+
+        public PaymentController(IPaymentGatewayService paymentGatewayService)
+        {
+            _paymentGatewayService= paymentGatewayService;
+        }
+        [HttpPost]
+        public IActionResult CreatePaymentUrl(PaymentInformationModel model)
+        {
+            var url = _paymentGatewayService.CreatePaymentUrlVnpay(model, HttpContext);
+
+            return Ok(url);
+        }
+    }
+}
---
+++ Repositories/Models/PaymentModels/PaymentResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using Repositories.Entities;
+using Repositories.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Models.PaymentModels
+{
+    public class PaymentResponseModel:BaseEntity
+    {
+        public Guid? Code { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public PaymentMethod? PaymentMethod { get; set; }
+        public bool Success { get; set; }
+        public string Token { get; set; }
+        public string VnPayResponseCode { get; set; }
+    }
+}
