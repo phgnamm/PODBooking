7f5e3d61093684951bfa5185629c30f28615ffb3
Thanh Dung <nguyenthanhdung2308@gmail.com>
2024-10-23 23:45:13 +0700

    update code

--- Services/Interfaces/IRewardPointService.cs
+++ Services/Interfaces/IRewardPointService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Repositories.Models.BookingModels;
+using Repositories.Models.RewardPointModels;
+using Services.Common;
+using Services.Models.BookingModels;
+using Services.Models.ResponseModels;
+using Services.Models.RewardPointModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +14,9 @@ namespace Services.Interfaces
 {
     public interface IRewardPointService
     {
+        Task<Pagination<RewardPointModel>> GetAllRewardPointsAsync(RewardPointFilterModel model);
+        Task<ResponseDataModel<RewardPointModel>> GetRewardByIdAsync(Guid id);
+        Task<ResponseModel> UpdateRewardPointAsync(Guid id, RewardPointUpdateModel model);
+        Task<ResponseModel> RemoveRewardPointAsync(Guid id);
     }
 }
