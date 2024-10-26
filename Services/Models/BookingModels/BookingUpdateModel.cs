01dbb16531aac2b2a523a8959baadd2da753ecac
phgnam <kiss01646803595@gmail.com>
2024-10-26 23:56:09 +0700

    fix : update booking

--- Services/Models/BookingModels/BookingUpdateModel.cs
+++ Services/Models/BookingModels/BookingUpdateModel.cs
@@ -10,15 +10,7 @@ namespace Services.Models.BookingModels
 {
     public class BookingUpdateModel
     {
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-        public bool UseRewardPoints { get; set; } 
-        public List<BookingServiceModelRequest> BookingServices { get; set; } = new List<BookingServiceModelRequest>();
-    }
-    public class BookingServiceModelRequest
-    {
-        public Guid ServiceId { get; set; }
-        public int Quantity { get; set; }
+       public bool UseRewardPoints { get; set; }
+
     }
 }
